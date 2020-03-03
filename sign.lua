fe5e46a7494ad314b053ac14a1406658d561b56b
Dejnarong Lamleangpon <dejnaronglamleangpon@Dejnarongs-MacBook-Pro.local>
2020-03-03 16:14:42 +0700

    first commit

---
+++ sign.lua
@@ -0,0 +1,70 @@
+openssl = require "openssl"
+base64 = require "base64"
+JSON = require("JSON")
+inspect = require("inspect")
+local config = require "config"
+
+pkey = config.key_data.provider1Wallet1Privkey
+provider1PrivateKeyRaw = config.key_data.Provider1PrivateKey
+provider1PrivateKey = nil
+
+b64Signature = nil
+jsonObj = {}
+sendObj = {}
+
+function init(args)
+  privKey = openssl.pkey.read(pkey, true)
+  provider1PrivateKey = openssl.pkey.read(provider1PrivateKeyRaw, true)
+  message = config.key_data.provider1Wallet1Pubkey
+  signatureStr = privKey.sign(privKey, message, "sha256")
+  --print(signatureStr)
+  b64Signature = base64.encode(signatureStr)
+  --print(b64Signature)
+  jsonObj = {
+    wallet_public_key = message,
+		wallet_public_key_signature = b64Signature
+  }
+  sendObj = JSON:encode(jsonObj)
+  --print(sendObj)
+end
+
+request = function()
+  nonce = openssl.random(32)
+  node_id = "provider1"
+  ts = os.time()
+
+  newMsg = node_id .. sendObj .. nonce .. ts
+  --print(newMsg)
+  newSignature = provider1PrivateKey.sign(provider1PrivateKey, newMsg, "sha256")
+  --print(signatureStr)
+  newb64Signature = base64.encode(newSignature)
+
+  path = "/api/create_wallet"
+  param = {
+    node_id = node_id,
+    signature = newb64Signature,
+    nonce = base64.encode(nonce),
+    timestamp = ts,
+    params = jsonObj
+  }
+  req_body = JSON:encode(param)
+  --print(req_body)
+  return wrk.format("POST", path, nil,req_body)
+end
+
+-- function response(status, headers, body)
+--   print(body)
+-- end
+
+
+
+-- account = config.objData
+-- json_text = JSON:encode(account)
+
+-- print(json_text)
+-- print(config.key_data.privateKey)
+-- node_id: nodeId,
+-- signature,
+-- nonce,
+-- timestamp: ts,
+-- params,
